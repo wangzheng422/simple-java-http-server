b4345602ce7f3be981a47df5c33718b6ed54792b
Wang Zheng <wangzheng422@gmail.com>
2024-07-05 22:44:21 +0800

    dump in line

--- src/main/java/com/example/httpservice/MyController.java
+++ src/main/java/com/example/httpservice/MyController.java
@@ -85,9 +85,9 @@ public class MyController {
         String dateTime = LocalDateTime.now().format(dtf);
         String dump_dir = System.getenv().getOrDefault("HEAP_DUMP_DIR", "/wzh-log");
         String dump_path = dump_dir + "/heap-dump_" + dateTime + ".hprof";
-        // dumpHeap(dump_path, true);
+        dumpHeap(dump_path, true);
         // Run dumpHeap in a separate thread
-        new Thread(() -> dumpHeap(dump_path, true)).start();
+        // new Thread(() -> dumpHeap(dump_path, true)).start();
 
         // Submit dumpHeap task to the executor service
         // executorService.submit(() -> dumpHeap(dump_path, true));
